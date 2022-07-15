8528537fe28842206e1121c2f5be223c17dc252c
Rene Mueller <muellren@gmail.com>
2022-07-15 19:07:01 +0200

    add asynchronous coroutine code

--- kotlin-client/src/main/kotlin/kotlinclient/App.kt
+++ kotlin-client/src/main/kotlin/kotlinclient/App.kt
@@ -1,6 +1,10 @@
 package kotlinclient
 
-import retrofit2.Call
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -25,54 +29,70 @@ data class Discount(val discount: Double)
 
 interface UserOrdersService {
     @GET("user/{user}")
-    fun getUser(@Path("user") user: Int): Call<User>
+    suspend fun getUser(@Path("user") user: Int): Response<User>
 
     @GET("orders")
-    fun getOrders(@Query("by") user: Int, @Query("limit") limit: Int): Call<LastOrders>
+    suspend fun getOrders(@Query("by") user: Int, @Query("limit") limit: Int): Response<LastOrders>
 }
 
 interface DiscountService {
     @GET("discount")
-    fun getDiscount(@Query("score") score: Int,
-                    @Query("orders") orders: Int,
-                    @Query("totals") totals: Double,
-                    @Query("first_order_date") firstOrderDate: String): Call<Discount>
+    suspend fun getDiscount(@Query("score") score: Int,
+                            @Query("orders") orders: Int,
+                            @Query("totals") totals: Double,
+                            @Query("first_order_date") firstOrderDate: String): Response<Discount>
 }
 
-fun main() {
+fun main() = runBlocking(context = Dispatchers.IO) {
     val userId = 12345678
     val limit = 5
 
+    // BUG Retrofit uses OkHttp internally as HTTP client, which in turn creates
+    // non-daemon threads preventing the VM from termination.
+    // WORKAROUND: explicitly create OkHttp client and also explicitly shut it down
+    // at the end.
+    val okHttpClient = OkHttpClient.Builder().build()
+
     val shopRetrofit: Retrofit = Retrofit.Builder()
         .baseUrl("http://localhost:4000/")
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
     val shopService: UserOrdersService = shopRetrofit.create(UserOrdersService::class.java)
 
-    val discountRetrofit: Retrofit = Retrofit.Builder()
-        .baseUrl("http://localhost:3000/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-    val discountService: DiscountService = discountRetrofit.create(DiscountService::class.java)
+    val deferredUser = async {
+        shopService
+            .getUser(12345678)
+            .body()
+    }
+    val deferredOrders = async {
+        shopService
+            .getOrders(user = userId, limit = limit)
+            .body()
+    }
 
-    val user = shopService
-        .getUser(12345678)
-        .execute()
-        .body()
-
-    val orders = shopService
-        .getOrders(user = userId, limit = limit)
-        .execute()
-        .body()
-
+    val user = deferredUser.await()
     val score = user?.score ?: 0
+    val orders = deferredOrders.await()
     val totals = orders?.last_orders?.sumOf { o: Order -> o.total } ?: 0.0
     val firstOrderDate = orders?.last_orders?.first()?.order_date ?: ""
 
-    val discountObj = discountService
-        .getDiscount(score, limit, totals, firstOrderDate)
-        .execute()
-        .body()
-    val discount = discountObj?.discount ?: 0.00
+    val discountObj = async {
+        val discountRetrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("http://localhost:3000/")
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val discountService: DiscountService =
+            discountRetrofit.create(DiscountService::class.java)
+        discountService
+            .getDiscount(score, limit, totals, firstOrderDate)
+            .body()
+    }
+    val discount = discountObj.await()?.discount ?: 0.0
     println("discount for user $userId: $discount")
+
+    // BUGFIX Terminate all non-daemon threads created by Retrofit's OkHttp client.
+    okHttpClient.dispatcher().executorService().shutdown()
+    okHttpClient.connectionPool().evictAll()
 }
